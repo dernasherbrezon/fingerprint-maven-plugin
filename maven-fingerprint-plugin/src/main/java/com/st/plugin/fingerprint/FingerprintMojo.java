32dc288240f8522306399ba76d884f5f7fad46fc
dernasherbrezon <dernasherbrezon@gmail.com>
2013-05-23 12:03:29 +0400

    initial

---
+++ maven-fingerprint-plugin/src/main/java/com/st/plugin/fingerprint/FingerprintMojo.java
@@ -0,0 +1,418 @@
+package com.st.plugin.fingerprint;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+
+/**
+ * @goal generate
+ * 
+ * @phase pre-package
+ */
+public class FingerprintMojo extends AbstractMojo {
+
+	private static final Pattern LINK_PATTERN = Pattern.compile("(<link.*?href=\")(.*?)(\".*?>)");
+	private static final Pattern SCRIPT_PATTERN = Pattern.compile("(\")([^\\s]*?\\.js)(\")");
+	private static final Pattern IMG_PATTERN = Pattern.compile("(<img.*?src=\")(.*?)(\".*?>)");
+	private static final Pattern CSS_IMG_PATTERN = Pattern.compile("(url\\(\")(.*?)(\"\\))");
+
+	/**
+	 * Output directory
+	 * 
+	 * @parameter expression="${project.build.directory}/fingered-web"
+	 * @required
+	 */
+	private File outputDirectory;
+
+	/**
+	 * Webapp directory
+	 * 
+	 * @parameter expression="${basedir}/src/main/webapp"
+	 * @required
+	 */
+	private File sourceDirectory;
+
+	/**
+	 * Exclude resources
+	 * 
+	 * @parameter
+	 * 
+	 */
+	private List<String> excludeResources;
+
+	/**
+	 * @parameter
+	 */
+	private List<String> extensionsToFilter;
+
+	/**
+	 * @parameter
+	 */
+	private Set<String> trimTagExtensions;
+
+	/**
+	 * CDN url
+	 * 
+	 * @parameter
+	 */
+	private String cdn;
+
+	private final Map<String, String> processedFiles = new HashMap<String, String>();
+
+	public void execute() throws MojoExecutionException {
+		if (!sourceDirectory.isDirectory()) {
+			throw new MojoExecutionException("source directory is not a directory: " + sourceDirectory.getAbsolutePath());
+		}
+		if (!outputDirectory.exists()) {
+			if (!outputDirectory.mkdirs()) {
+				throw new MojoExecutionException("unable to create outputdirectory: " + outputDirectory.getAbsolutePath());
+			}
+		}
+		if (!outputDirectory.isDirectory()) {
+			throw new MojoExecutionException("output directory is not a directory: " + outputDirectory.getAbsolutePath());
+		}
+		List<File> pagesToFilter = new ArrayList<File>();
+		findPagesToFilter(pagesToFilter, sourceDirectory);
+		if (pagesToFilter.isEmpty()) {
+			return;
+		}
+
+		copyDirectories(sourceDirectory, outputDirectory);
+
+		for (File curHTml : pagesToFilter) {
+			processPage(curHTml);
+		}
+
+		try {
+			copyDeepFiles(sourceDirectory, outputDirectory);
+		} catch (IOException e) {
+			throw new MojoExecutionException("unable to deep copy files", e);
+		}
+	}
+
+	private void processPage(File file) throws MojoExecutionException {
+		File fileToProcess;
+		if (processedFiles.containsKey(file.getAbsolutePath())) {
+			fileToProcess = new File(processedFiles.get(file.getAbsolutePath()));
+		} else {
+			fileToProcess = file;
+		}
+		getLog().info("processing file: " + fileToProcess.getAbsolutePath());
+		String data = readFile(fileToProcess);
+		StringBuffer outputFileData = processPattern(LINK_PATTERN, data);
+		outputFileData = processPattern(SCRIPT_PATTERN, outputFileData.toString());
+		outputFileData = processPattern(IMG_PATTERN, outputFileData.toString());
+		outputFileData = processPattern(CSS_IMG_PATTERN, outputFileData.toString());
+		String processedData = null;
+		if (trimTagExtensions != null && !trimTagExtensions.isEmpty()) {
+			String extension = getExtension(fileToProcess.getName());
+			if (extension != null && trimTagExtensions.contains(extension)) {
+				processedData = TextShrinker.shrink(outputFileData.toString());
+			}
+		}
+
+		if (processedData == null) {
+			processedData = outputFileData.toString();
+		}
+
+		FileWriter w = null;
+		File targetFile;
+		if (!processedFiles.containsKey(file.getAbsolutePath())) {
+			String targetHtmlFilename = generateTargetFilename(sourceDirectory, fileToProcess);
+			targetFile = new File(outputDirectory, targetHtmlFilename);
+		} else {
+			targetFile = fileToProcess;
+		}
+		try {
+			w = new FileWriter(targetFile);
+			w.append(processedData);
+			w.flush();
+		} catch (IOException e) {
+			throw new MojoExecutionException("unable to write html file: " + targetFile.getAbsolutePath(), e);
+		} finally {
+			if (w != null) {
+				try {
+					w.close();
+				} catch (IOException e) {
+					getLog().warn("unable to close file cursor", e);
+				}
+			}
+		}
+		if (!processedFiles.containsKey(file.getAbsolutePath())) {
+			processedFiles.put(file.getAbsolutePath(), targetFile.getAbsolutePath());
+		}
+	}
+
+	private StringBuffer processPattern(Pattern p, String data) throws MojoExecutionException {
+		StringBuffer outputFileData = new StringBuffer();
+		Matcher m = p.matcher(data);
+		while (m.find()) {
+			String curLink = m.group(2);
+			if (isExcluded(curLink)) {
+				getLog().info("resource excluded: " + curLink);
+				m.appendReplacement(outputFileData, "$1" + curLink + "$3");
+				continue;
+			}
+			int queryIndex = curLink.indexOf("?");
+			String query = "";
+			if (queryIndex != -1) {
+				query = curLink.substring(queryIndex);
+				curLink = curLink.substring(0, queryIndex);
+			} else {
+				queryIndex = curLink.indexOf("#");
+				if (queryIndex != -1) {
+					query = curLink.substring(queryIndex);
+					curLink = curLink.substring(0, queryIndex);
+				}
+			}
+
+			File linkFile = new File(sourceDirectory, curLink);
+			if (!linkFile.exists()) {
+				getLog().warn("resource file doesnt exist: " + curLink + " file: " + linkFile.getName());
+				curLink = curLink.replaceAll("\\$", "\\\\\\$");
+				m.appendReplacement(outputFileData, "$1" + curLink + "$3");
+				continue;
+			}
+			if (curLink.length() > 0 && curLink.charAt(0) != '/') {
+				getLog().warn("resource has relative path: " + curLink);
+			}
+			String fingerprint = generateFingerprint(readBinaryFile(linkFile));
+			String targetPath = generateTargetResourceFilename(fingerprint, curLink);
+			if (targetPath.length() != 0 && targetPath.charAt(0) != '/') {
+				getLog().warn("relative path detected: " + curLink);
+			}
+
+			String targetURL;
+			if (cdn == null) {
+				targetURL = "$1" + targetPath + query + "$3";
+			} else {
+				targetURL = "$1" + cdn + targetPath + query + "$3";
+			}
+
+			m.appendReplacement(outputFileData, targetURL);
+			File targetFilename = new File(outputDirectory, targetPath);
+			if (targetFilename.exists()) {
+				getLog().info("processing link: " + linkFile.getAbsolutePath());
+				continue;
+			}
+			getLog().info("processing link: " + linkFile.getAbsolutePath() + " copy to: " + targetFilename.getAbsolutePath());
+			if (processedFiles.containsKey(linkFile.getAbsolutePath())) {
+				String pathWithinSource = linkFile.getAbsolutePath();
+				linkFile = new File(processedFiles.get(pathWithinSource));
+				processedFiles.put(pathWithinSource, targetFilename.getAbsolutePath());
+				try {
+					copy(new FileInputStream(linkFile), new FileOutputStream(targetFilename), 2048);
+				} catch (Exception e) {
+					throw new MojoExecutionException("unable to copy resource file: " + linkFile + " to: " + targetFilename, e);
+				}
+				if (!linkFile.delete()) {
+					getLog().warn("unable to move " + linkFile.getAbsolutePath());
+				}
+			} else {
+				processedFiles.put(linkFile.getAbsolutePath(), targetFilename.getAbsolutePath());
+				try {
+					copy(new FileInputStream(linkFile), new FileOutputStream(targetFilename), 2048);
+				} catch (Exception e) {
+					throw new MojoExecutionException("unable to copy resource file: " + linkFile + " to: " + targetFilename, e);
+				}
+			}
+		}
+		m.appendTail(outputFileData);
+		return outputFileData;
+	}
+
+	private boolean isExcluded(String path) {
+		if (excludeResources == null) {
+			return false;
+		}
+		for (String curExclude : excludeResources) {
+			if (path.contains(curExclude)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private static String generateFingerprint(byte[] data) throws MojoExecutionException {
+		MessageDigest md5Alg;
+		try {
+			md5Alg = MessageDigest.getInstance("MD5");
+			md5Alg.reset();
+			byte[] digest = md5Alg.digest(data);
+			BigInteger result = new BigInteger(digest);
+			String resultStr = null;
+			if (result.signum() < 0) {
+				resultStr = result.negate().toString(16);
+			} else {
+				resultStr = result.toString(16);
+			}
+			return resultStr;
+		} catch (NoSuchAlgorithmException e) {
+			throw new MojoExecutionException("unable to generate fingerprint", e);
+		}
+	}
+
+	static String generateTargetResourceFilename(String fingerprint, String sourceFilename) {
+		int index = sourceFilename.lastIndexOf("/");
+		if (index == -1) {
+			return fingerprint + sourceFilename;
+		}
+		String filename = sourceFilename.substring(index + 1);
+		return sourceFilename.substring(0, index) + "/" + fingerprint + filename;
+	}
+
+	static String generateTargetFilename(File sourceDirectory, File file) {
+		return file.getAbsolutePath().substring(sourceDirectory.getAbsolutePath().length());
+	}
+
+	private void copyDirectories(File curSrcDirectory, File curDestDirectory) {
+		if (!curSrcDirectory.isDirectory()) {
+			return;
+		}
+		File[] subFiles = curSrcDirectory.listFiles();
+		for (File curFile : subFiles) {
+			if (!curFile.isDirectory()) {
+				continue;
+			}
+			File newDir = new File(curDestDirectory, curFile.getName());
+			if (!newDir.exists()) {
+				if (!newDir.mkdirs()) {
+					getLog().warn("unable to create directory in outputDirectory: " + newDir);
+					continue;
+				}
+			}
+			copyDirectories(curFile, newDir);
+		}
+	}
+
+	private void copyDeepFiles(File srcDir, File dstDir) throws IOException {
+		File[] srcFiles = srcDir.listFiles();
+		for (File curFile : srcFiles) {
+			if (curFile.isDirectory()) {
+				copyDeepFiles(curFile, new File(dstDir, curFile.getName()));
+				continue;
+			}
+
+			if (processedFiles.containsKey(curFile.getAbsolutePath())) {
+				continue;
+			}
+
+			copy(new FileInputStream(curFile), new FileOutputStream(new File(dstDir, curFile.getName())), 2048);
+		}
+	}
+
+	private String readFile(File file) throws MojoExecutionException {
+		BufferedReader r = null;
+		String curLine = null;
+		StringBuilder builder = new StringBuilder();
+		try {
+			r = new BufferedReader(new FileReader(file));
+			while ((curLine = r.readLine()) != null) {
+				builder.append(curLine);
+				builder.append("\n");
+			}
+		} catch (Exception e) {
+			throw new MojoExecutionException("unable to read file: " + file, e);
+		} finally {
+			if (r != null) {
+				try {
+					r.close();
+				} catch (IOException e) {
+					getLog().warn("unable to close file cursor: " + file, e);
+				}
+			}
+		}
+		return builder.toString();
+	}
+
+	private byte[] readBinaryFile(File f) throws MojoExecutionException {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		FileInputStream fis = null;
+		try {
+			fis = new FileInputStream(f);
+			byte[] buf = new byte[2048];
+			int bytesRead = -1;
+			while ((bytesRead = fis.read(buf)) != -1) {
+				baos.write(buf, 0, bytesRead);
+			}
+			return baos.toByteArray();
+		} catch (Exception e) {
+			throw new MojoExecutionException("unable to read file: " + f, e);
+		} finally {
+			if (fis != null) {
+				try {
+					fis.close();
+				} catch (IOException e) {
+					getLog().warn("unable to close file cursor: " + f, e);
+				}
+			}
+		}
+	}
+
+	private static void copy(InputStream inputStream, OutputStream outputStream, int bufferSize) throws IOException {
+		byte[] buffer = new byte[bufferSize];
+		int n;
+		while (-1 != (n = inputStream.read(buffer))) {
+			outputStream.write(buffer, 0, n);
+		}
+		inputStream.close();
+		outputStream.close();
+	}
+
+	private void findPagesToFilter(List<File> output, File source) {
+		if (!source.isDirectory()) {
+			return;
+		}
+		File[] subFiles = source.listFiles();
+		for (File curFile : subFiles) {
+			if (curFile.isDirectory()) {
+				findPagesToFilter(output, curFile);
+				continue;
+			}
+
+			if (!curFile.isFile()) {
+				continue;
+			}
+
+			String extension = getExtension(curFile.getName());
+			if (extension == null) {
+				continue;
+			}
+
+			if (extensionsToFilter.contains(extension)) {
+				output.add(curFile);
+				continue;
+			}
+		}
+	}
+
+	private static String getExtension(String filename) {
+		int extensionIndex = filename.lastIndexOf(".");
+		if (extensionIndex == -1) {
+			return null;
+		}
+		String extension = filename.substring(extensionIndex + 1);
+		return extension;
+	}
+
+}
---
+++ maven-fingerprint-plugin/src/main/java/com/st/plugin/fingerprint/TextShrinker.java
@@ -0,0 +1,63 @@
+package com.st.plugin.fingerprint;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+class TextShrinker {
+
+	private final static Pattern newLineSymbols = Pattern.compile("[\n\t\r]");
+	private final static Pattern tagWhiteSpaces = Pattern.compile(">(\\s+)<");
+	private final static String PRE_START_TAG = "<pre";
+	private final static String PRE_END_TAG = "</pre>";
+	private final static Pattern DEFAULT_INPUT_TYPE = Pattern.compile("(<input.*?) type=\"text\"");
+
+	static String shrink(String page) {
+		Matcher m = DEFAULT_INPUT_TYPE.matcher(page);
+		
+		StringBuffer sb = new StringBuffer();
+		while( m.find() ) {
+			m.appendReplacement(sb, "$1");
+		}
+		m.appendTail(sb);
+		page = sb.toString();
+		if (!page.contains(PRE_START_TAG)) {
+			return fix(page);
+		}
+		
+		StringBuilder b = new StringBuilder();
+		List<String> parts = getParts(0, page);
+		for (String curPart : parts) {
+			if (curPart.contains(PRE_START_TAG)) {
+				b.append(curPart);
+			} else {
+				b.append(fix(curPart));
+			}
+		}
+		return b.toString();
+	}
+
+	private static String fix(String original) {
+		String page = newLineSymbols.matcher(original).replaceAll("");
+		String result = tagWhiteSpaces.matcher(page).replaceAll("><");
+		return result.trim();
+	}
+
+	private static List<String> getParts(int curStart, String original) {
+		List<String> result = new ArrayList<String>();
+		int start = original.indexOf(PRE_START_TAG, curStart);
+		if (start == -1) {
+			result.add(original.substring(curStart));
+			return result;
+		}
+		int end = original.indexOf(PRE_END_TAG, start);
+		int newStart = end + PRE_END_TAG.length();
+		result.add(original.substring(curStart, start));
+		result.add(original.substring(start, newStart));
+		List<String> subs = getParts(newStart, original);
+		result.addAll(subs);
+		return result;
+	}
+
+}
