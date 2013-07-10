bd527d29f6d07d0d73fbb87e682ad25680cdfb58
Richard Scott Smith <pilbender@gmail.com>
2013-07-10 14:26:26 -0700

    Enhanced the JUnit test for matching more CSS image patterns.  Also added simple tests for the other patterns. More of these tests should be added in the future.

--- maven-fingerprint-plugin/src/test/java/com/st/plugin/fingerprint/FingerprintMojoTest.java
+++ maven-fingerprint-plugin/src/test/java/com/st/plugin/fingerprint/FingerprintMojoTest.java
@@ -1,10 +1,6 @@
 package com.st.plugin.fingerprint;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.*;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -37,15 +33,36 @@ public class FingerprintMojoTest {
 
 	}
 
+	/**
+	 * Testing of the various regex patterns.
+	 * @throws Exception
+	 */
 	@Test
 	public void testPattern() throws Exception {
 		System.out.println("Patterns Test");
 
+		Pattern linkPattern = fingerprintMojo.LINK_PATTERN;
+		String linkUrl = "<link rel=\"stylesheet\" href=\"${basePath}/resources/css/style.css\" />";
+		Matcher linkMatcher = linkPattern.matcher(linkUrl);
+		assertTrue(linkMatcher.find());
+
+		Pattern scriptPattern = fingerprintMojo.SCRIPT_PATTERN;
+		String scriptUrl = "<script src=\"${basePath}/resources/js/vendor/zepto.js\">";
+		Matcher scriptMatcher = scriptPattern.matcher(scriptUrl);
+		assertTrue(scriptMatcher.find());
+
+		Pattern imgPattern = fingerprintMojo.IMG_PATTERN;
 		String imageUrl = "<img src=\"/images/favicon-whatever.ico\" />";
+		Matcher imgMatcher = imgPattern.matcher(imageUrl);
+		assertTrue(imgMatcher.find());
 
-		Pattern pattern = fingerprintMojo.IMG_PATTERN;
-		Matcher matcher = pattern.matcher(imageUrl);
+		Pattern cssPattern = fingerprintMojo.CSS_IMG_PATTERN;
+		String cssUrl1 = "url(\"/images/navigation-s66728e073e.png\")";
+		Matcher cssMatcher1 = cssPattern.matcher(cssUrl1);
+		assertTrue(cssMatcher1.find());
+		String cssUrl2 = "url('/images/navigation-s66728e073e.png')";
+		Matcher cssMatcher2 = cssPattern.matcher(cssUrl2);
+		assertTrue(cssMatcher2.find());
 
-		assertTrue(matcher.find());
 	}
 }
